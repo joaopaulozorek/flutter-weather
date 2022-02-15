712b1b604b0e60d0abcfe447418122b7e0154534
João Paulo Zorek <joaopaulozorek@gmail.com>
2022-02-14 22:34:22 -0300

    adicionando o método get Data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_weather_app/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -9,8 +10,23 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
-    Location location = new Location();
+    Location location = Location();
     await location.getCurrentLocation();
+
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getData() async {
+    http.Response response = await http
+        .get(Uri.parse('https://apiprevmet3.inmet.gov.br/previsao/5300108'));
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
@@ -21,6 +37,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    getData();
+    return const Scaffold();
   }
 }
